ce79b737620cb4c8d8cd915e534d828f3cd40060
Angela To <to.angelac@gmail.com>
2016-06-23 14:22:47 -0700

    updated "solution"

--- filterCR.vb
+++ filterCR.vb
@@ -3,7 +3,7 @@
     ' make sure the first "Project" type that corresponds with that PID is a
     ' "Project" type
     ' Note: Type=0 : Other type   (Dont Care)
-    '       Type=1 : Project type 
+    '       Type=1 : Project type
     
     Sub FilterSolutions_Click()
         Dim i, j As Integer
@@ -13,15 +13,15 @@
             cur_id = Cells(i, 1)
             cur_type = Cells(i, 2)
             ' 1. Unique Project ID begins with Project Type
-            If cur_type = 1 Then
+            If cur_type = "Solution" Then
                 If i > 2 Then
                     j = i - 1
                     prev_id = Cells(j, 1)
                     prev_type = Cells(j, 2)
-                    If (prev_id = cur_id And prev_type <> 1) Then
+                    If (prev_id = cur_id And prev_type <> "Solution") Then
                         ' 4. Continue iterating backwards to check whether
                         '    rows need to be removed
-                        Do While (prev_id = cur_id And prev_type <> 1)
+                        Do While (prev_id = cur_id And prev_type <> "Solution")
                             Rows(j).EntireRow.Delete
                             j = j - 1
                             prev_id = Cells(j, 1)
@@ -34,4 +34,3 @@
         Loop
     End Sub
         
-
