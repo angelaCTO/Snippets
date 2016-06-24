bbfc7c26670a827ada6047d328ab567c00cd1e70
Angela To <to.angelac@gmail.com>
2016-06-24 11:27:00 -0700

    Update filterCR.vb

--- filterCR.vb
+++ filterCR.vb
@@ -1,36 +1,35 @@
     ' Objective:
-    ' Iterate through rows, for each unique PID found (made easy since PIDS are ordered)
-    ' make sure the first "Project" type that corresponds with that PID is a
-    ' "Project" type
-    ' Note: Type=0 : Other type   (Dont Care)
-    '       Type=1 : Project type
+    ' Iterate through rows, for each unique CR_ID (ordered) found
+    ' make sure the first "Solution" type that corresponds with that CR_ID is a
+    ' "Solution" type
+
     
     Sub FilterSolutions_Click()
         Dim i, j As Integer
         i = 2
         
+        ' Remove "non-solutions" on CR_ID criteria
         Do Until IsEmpty(Cells(i, 1))
             cur_id = Cells(i, 1)
-            cur_type = Cells(i, 2)
-            ' 1. Unique Project ID begins with Project Type
+            cur_type = Cells(i, 12)
             If cur_type = "Solution" Then
                 If i > 2 Then
                     j = i - 1
                     prev_id = Cells(j, 1)
-                    prev_type = Cells(j, 2)
+                    prev_type = Cells(j, 12)
                     If (prev_id = cur_id And prev_type <> "Solution") Then
-                        ' 4. Continue iterating backwards to check whether
-                        '    rows need to be removed
                         Do While (prev_id = cur_id And prev_type <> "Solution")
                             Rows(j).EntireRow.Delete
                             j = j - 1
                             prev_id = Cells(j, 1)
-                            prev_type = Cells(j, 2)
+                            prev_type = Cells(j, 12)
                         Loop
                     End If
                 End If
             End If
             i = i + 1
         Loop
-    End Sub
         
+        ' Remove Duplicates on CR_ID critera
+        Cells.RemoveDuplicates Columns:=Array(1)
+    End Sub
