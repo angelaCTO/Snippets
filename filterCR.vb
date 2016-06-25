535b77c3827ea80ed660cd64ffeaaee11ac31727
Angela To <to.angelac@gmail.com>
2016-06-24 18:11:39 -0700

    :)

--- filterCR.vb
+++ filterCR.vb
@@ -1,37 +1,40 @@
-' Objective:
-' Iterate through rows, for each unique CR_ID (ordered) found
-' make sure the first "Solution" type that corresponds with that CR_ID is a
-' "Solution" type
+Private Sub process_all_crs_Click()
 
-Sub FilterSolutions_Click()
-    ' Filter BP 1
-    ThisWorkbook.Sheets("test_sheet").Activate
-
-    ' Declare/initiate our iterators
+    Dim ws As Worksheet
     Dim i, j As Integer
-    i = 2
     
-    ' Remove "non-solutions" on CR_ID criteria
-    Do Until IsEmpty(Cells(i, 1))
-        cur_id = Cells(i, 1)
-        cur_type = Cells(i, 12)
-        If cur_type = "Solution" Then
-            If i > 2 Then
-                j = i - 1
-                prev_id = Cells(j, 1)
-                prev_type = Cells(j, 12)
-                If (prev_id = cur_id And prev_type <> "Solution") Then
-                    Do While (prev_id = cur_id And prev_type <> "Solution")
-                        Rows(j).EntireRow.Delete
-                        j = j - 1
-                        prev_id = Cells(j, 1)
-                        prev_type = Cells(j, 12)
-                    Loop
-                End If
-            End If
-        End If
-        i = i + 1
-    Loop
-    ' Remove Duplicates on CR_ID critera
-    Cells.RemoveDuplicates Columns:=Array(1)
+    ' Process selected BP CR sheets
+    For Each ws In Worksheets
+        Select Case ws.CodeName
+          ' Note here, sheet "Name" may not be the same as its CodeName
+          ' which is immutable
+          '      "1st BP CRs", "2nd BP CRs", "3rd BP CRs", "4th BP CRs"
+            Case "Sheet5", "Sheet8", "Sheet111", "Sheet14"
+            With ws
+                i = 2 ' Skip the header
+                ' Iterate through the rows
+                Do Until IsEmpty(.Cells(i, 1))
+                    cur_id = .Cells(i, 1)
+                    cur_type = .Cells(i, 12)
+                    If cur_type = "Solution" And i > 2 Then
+                            j = i - 1
+                            prev_id = .Cells(j, 1)
+                            prev_type = .Cells(j, 12)
+                            ' Remove Non-Solutions on CR_ID criteria
+                            If (prev_id = cur_id And prev_type <> "Solution") Then
+                                Do While (prev_id = cur_id And prev_type <> "Solution")
+                                    .Rows(j).EntireRow.Delete
+                                    j = j - 1
+                                    prev_id = .Cells(j, 1)
+                                    prev_type = .Cells(j, 12)
+                                Loop
+                            End If
+                    End If
+                    i = i + 1
+                Loop
+                ' Remove Duplicates on CR_ID critera
+                .Cells.RemoveDuplicates Columns:=Array(1)
+            End With
+        End Select
+    Next ws
 End Sub
