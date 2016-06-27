c0f1ab2e5466d5b7f395c78ff13ac76cc6f78dd4
Angela To <to.angelac@gmail.com>
2016-06-27 12:26:35 -0700

    hacky

--- filterCR.vb
+++ filterCR.vb
@@ -3,6 +3,8 @@ Private Sub process_all_crs_Click()
     Dim bp_cr As Worksheet
     Dim i As Integer, j As Integer
     
+    Dim h_i As Integer ' TODO remove after refractor
+    
     ' Optimization Attempt - Disable all automatic events until all sheets
     ' have been processed. Counts will update automatically upon completion
     With Application
@@ -14,30 +16,43 @@ Private Sub process_all_crs_Click()
         ' non-solutions on CR_ID criteria then removing duplicates
         For Each bp_cr In Worksheets
             ' Note here, sheet "Name" may not be the same as its "CodeName"
-            ' which is immutable (this is best)
+            ' which is immutable (this is best) as future revisions will
+            ' not affect sheet coding. The following naming correspondence
+            ' "1st BP CRs" = Sheet5
+            ' "2nd BP CRs" = Sheet8
+            ' "3rd BP CRs" = Sheet111
+            ' "4th BP CRs" = Sheet14
             Select Case bp_cr.CodeName
-                ' -> "1st BP CRs", "2nd BP CRs", "3rd BP CRs", "4th BP CRs"
                 Case "Sheet5", "Sheet8", "Sheet111", "Sheet14"
                 With bp_cr
-                    i = 2
-                    Do Until IsEmpty(.Cells(i, 1))
-                        cur_id = .Cells(i, 1)
-                        cur_type = .Cells(i, 12)
-                        If cur_type = "Solution" And i > 2 Then
-                                j = i - 1
-                                prev_id = .Cells(j, 1)
-                                prev_type = .Cells(j, 12)
-                                If (prev_id = cur_id And prev_type <> "Solution") Then
+                
+                    ' Dev Note, (temp) dirty hack, run two loops before remDups
+                    ' refractor later then remove
+                    For h_i = 1 To 2
+                        ' Main Loop -------------------------------------------
+                        i = 2
+                        Do Until IsEmpty(.Cells(i, 1))
+                            cur_id = .Cells(i, 1)
+                            cur_type = .Cells(i, 12)
+                        
+                            If cur_type = "Solution" And i > 2 Then
+                                 j = i - 1
+                                 prev_id = .Cells(j, 1)
+                                 prev_type = .Cells(j, 12)
                                     Do While (prev_id = cur_id And prev_type <> "Solution")
                                         .Rows(j).EntireRow.Delete
                                         j = j - 1
                                         prev_id = .Cells(j, 1)
                                         prev_type = .Cells(j, 12)
                                     Loop
-                                End If
-                        End If
-                        i = i + 1
-                    Loop
+                            End If
+                            i = i + 1
+                        Loop
+                        ' End Main Loop ---------------------------------------
+                    ' Hacky patch - refractor later
+                    Next h_i
+                    
+                    ' Refractor
                     .Cells.RemoveDuplicates Columns:=Array(1)
                 End With
             End Select
