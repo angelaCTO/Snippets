516212ff9203be9342c599990fb567986f34b843
Angela To <to.angelac@gmail.com>
2016-06-27 14:02:05 -0700

    "documentation"

--- filterCR.vb
+++ filterCR.vb
@@ -1,40 +1,49 @@
+' DOCS:
+' -----------------------------------------------------------------------------
+' Processes all BP CRs 1-4 sheets by first iterating through records
+' Once a unique CR_ID has been found, ensure that it is of WR_Type
+' Solution, else backtrack, remove all Non-Solutions that have
+' matching CR_IDs, then remove duplicate records by CR_ID criteria
+'
+' NOTES:
+'   Note(1), Optimization Attempt - Disable all automatic application events
+'   until all sheets have been processed. Counts will update automatically
+'   upon completion. Once script has completed, application settings will
+'   to its original state
+'
+'   Note(2), sheet "Name" may not be the same as its "CodeName"
+'   which is immutable (this is best) as future revisions will
+'   not affect sheet coding. The following naming correspondence
+'   "1st BP CRs" = Sheet5
+'   "2nd BP CRs" = Sheet8
+'   "3rd BP CRs" = Sheet111
+'   "4th BP CRs" = Sheet14
+'
+'   Dev Note, Need to refractor and modularaize (when time permits)
+'   Dirty hack runs two loops before remDups (h_i). Investigation required
+'------------------------------------------------------------------------------
 Private Sub process_all_crs_Click()
 
     Dim bp_cr As Worksheet
     Dim i As Integer, j As Integer
+    Dim h_i As Integer ' TODO - Remove a/f Refact
     
-    Dim h_i As Integer ' TODO remove after refractor
-    
-    ' Optimization Attempt - Disable all automatic events until all sheets
-    ' have been processed. Counts will update automatically upon completion
+    ' Refer To Note(1)
     With Application
         .Calculation = xlCalculationManual
         .ScreenUpdating = False
         .EnableEvents = False
         
-        ' Process selected BP CR sheets (skip the header) first removing
-        ' non-solutions on CR_ID criteria then removing duplicates
+        ' Refer To Note(2)
         For Each bp_cr In Worksheets
-            ' Note here, sheet "Name" may not be the same as its "CodeName"
-            ' which is immutable (this is best) as future revisions will
-            ' not affect sheet coding. The following naming correspondence
-            ' "1st BP CRs" = Sheet5
-            ' "2nd BP CRs" = Sheet8
-            ' "3rd BP CRs" = Sheet111
-            ' "4th BP CRs" = Sheet14
             Select Case bp_cr.CodeName
                 Case "Sheet5", "Sheet8", "Sheet111", "Sheet14"
                 With bp_cr
-                
-                    ' Dev Note, (temp) dirty hack, run two loops before remDups
-                    ' refractor later then remove
-                    For h_i = 1 To 2
-                        ' Main Loop -------------------------------------------
+                    For h_i = 1 To 2 ' TODO - Remove a/f Refact
                         i = 2
                         Do Until IsEmpty(.Cells(i, 1))
                             cur_id = .Cells(i, 1)
                             cur_type = .Cells(i, 12)
-                        
                             If cur_type = "Solution" And i > 2 Then
                                  j = i - 1
                                  prev_id = .Cells(j, 1)
@@ -48,17 +57,12 @@ Private Sub process_all_crs_Click()
                             End If
                             i = i + 1
                         Loop
-                        ' End Main Loop ---------------------------------------
-                    ' Hacky patch - refractor later
-                    Next h_i
-                    
-                    ' Refractor
+                    Next h_i ' TODO - Remove a/f Refact
                     .Cells.RemoveDuplicates Columns:=Array(1)
                 End With
             End Select
         Next bp_cr
         
-        ' Return Application To Original State
         .Calculation = xlCalculationAutomatic
         .ScreenUpdating = True
         .EnableEvents = True
