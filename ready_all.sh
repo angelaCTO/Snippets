351d267bdfc3c568a3074a196b3c4e088dd5c58e
Angela To <angela.to2@boeing.com>
2020-02-11 10:47:11 -0800

    restructured

--- ready_all.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# untar all files (dir)
-for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
-
-# unpackage all files (subdir)
-find . -type d -exec sh -c '(cd {} && (for a in `ls -l *.rpm`; do sudo rpm -ivh $a; done))' ';'
